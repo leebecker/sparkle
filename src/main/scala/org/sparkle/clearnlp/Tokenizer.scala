6758b9a504d52f1c0228954687b4b3fe4a5e3d6d
leebecker <lee.becker@gmail.com>
2016-01-07 22:55:41 -0700

    Modified ClearNLP segmenter/tokenizer
    Now works with updated Slabs taken from epic

--- src/main/scala/org/sparkle/clearnlp/Tokenizer.scala
+++ src/main/scala/org/sparkle/clearnlp/Tokenizer.scala
@@ -5,10 +5,9 @@ import java.io.InputStream
 import edu.emory.clir.clearnlp.component.utils.NLPUtils
 import edu.emory.clir.clearnlp.tokenization.AbstractTokenizer
 import edu.emory.clir.clearnlp.util.lang.TLanguage
+import epic.slab._
+import epic.trees.Span
 import org.apache.commons.io.IOUtils
-import org.sparkle.slab.Slab.StringSlab
-import org.sparkle.typesystem.Span
-import org.sparkle.typesystem.basic.{Token, Sentence}
 
 import scala.collection.mutable.ListBuffer
 import scala.collection.JavaConversions._
@@ -17,12 +16,22 @@ import scala.collection.JavaConversions._
   * Created by leebecker on 1/7/16.
   */
 
-object Tokenizer {
-  // FIXME parameterize language code and pre-load tokenizer
-  val languageCode = "ENGLISH"
-  val tokenizer = NLPUtils.getTokenizer(TLanguage.getType(languageCode));
+trait SentenceSegmenterAndTokenizerBase extends StringAnalysisFunction[Any, Sentence with Token] with (String => Iterable[String]) with Serializable {
+  override def toString = getClass.getName
 
-  def sentenceSegmenterAndTokenizer[AnnotationTypes <: Span](slab: StringSlab[AnnotationTypes]) = {
+  def apply(a: String):IndexedSeq[String] = {
+    val slab = Slab(a)
+    apply(slab).iterator[Sentence with Token].toIndexedSeq.map(s => slab.spanned(s._1))
+  }
+}
+
+object SentenceSegmenterAndTokenizer extends SentenceSegmenterAndTokenizerBase {
+  // FIXME parameterize language code and pre-load tokenizer
+  val defaultLanguageCode = TLanguage.ENGLISH.toString
+  val tokenizer = NLPUtils.getTokenizer(TLanguage.getType(defaultLanguageCode));
+
+  override def apply[In](slab: StringSlab[In]): StringSlab[In with Sentence with Token] =  {
+
     // Convert slab text to an input stream and run with ClearNLP
     val stream: InputStream = IOUtils.toInputStream(slab.content);
     val sentencesAsTokens = tokenizer.segmentize(stream)
@@ -31,12 +40,12 @@ object Tokenizer {
     val sentenceAsTokenSpans = sentencesAsTokens.map(sentenceTokens => {
       var offset = 0
       slab.content.indexOf()
-      val tokens = new ListBuffer[Token]()
+      val tokens = new ListBuffer[Tuple2[Span, Token]]()
       for (tokenString:String <- sentenceTokens) {
         val tokenBegin = slab.content.indexOf(tokenString, offset)
         val tokenEnd = tokenBegin + tokenString.length
         if (tokenBegin >= 0 && tokenEnd >= 0) {
-          tokens += Token(tokenBegin, tokenEnd)
+          tokens += Tuple2(Span(tokenBegin, tokenEnd), Token(slab.content.substring(tokenBegin, tokenEnd)))
         }
         offset = tokenEnd
       }
@@ -44,10 +53,61 @@ object Tokenizer {
     })
 
     // Take the head and tail of each sentence to get the sentence offsets
-    val sentenceSpans = sentenceAsTokenSpans.map(sentenceTokenSpans => Sentence(sentenceTokenSpans.head.begin,sentenceTokenSpans.last.end))
+    val sentenceSpans = sentenceAsTokenSpans.map(sentenceTokenSpans =>
+      Tuple2(Span(sentenceTokenSpans.head._1.begin, sentenceTokenSpans.last._1.end), Sentence()))
+
+    // Flatten all the tokens into a single list
     val tokenSpans = sentenceAsTokenSpans.flatMap(t=> t)
 
     // Create new Add annotations to a
-    slab ++ sentenceSpans.iterator ++ tokenSpans.iterator
+    //if(start != slab.content.length)
+    //spans += Span(start, slab.content.length) -> Token(slab.content.substring(start, slab.content.length))
+    var s = slab.addLayer[Sentence](sentenceSpans).addLayer[Token](tokenSpans)
+    s
+    //slab
   }
 }
+
+//object SentenceSegmenterAndTokenizer extends StringAnalysisFunction[Any, Any] {
+//  // FIXME parameterize language code and pre-load tokenizer
+//  val defaultLanguageCode = TLanguage.ENGLISH.toString
+//  val tokenizer = NLPUtils.getTokenizer(TLanguage.getType(defaultLanguageCode));
+//
+//
+//  //def sentenceSegmenterAndTokenizer[AnnotationTypes <: Span](slab: StringSlab[AnnotationTypes]) = {
+//  override def apply[In](slab: StringSlab[In]) = {
+//    // Convert slab text to an input stream and run with ClearNLP
+//    val stream: InputStream = IOUtils.toInputStream(slab.content);
+//    val sentencesAsTokens = tokenizer.segmentize(stream)
+//
+//    // Convert token strings in each sentence into Token span annotations
+//    val sentenceAsTokenSpans = sentencesAsTokens.map(sentenceTokens => {
+//      var offset = 0
+//      slab.content.indexOf()
+//      val tokens = new ListBuffer[Tuple2[Span, Token]]()
+//      for (tokenString:String <- sentenceTokens) {
+//        val tokenBegin = slab.content.indexOf(tokenString, offset)
+//        val tokenEnd = tokenBegin + tokenString.length
+//        if (tokenBegin >= 0 && tokenEnd >= 0) {
+//          tokens += Tuple2(Span(tokenBegin, tokenEnd), Token(slab.content.substring(tokenBegin, tokenEnd)))
+//        }
+//        offset = tokenEnd
+//      }
+//      tokens.toList
+//    })
+//
+//    // Take the head and tail of each sentence to get the sentence offsets
+//    val sentenceSpans = sentenceAsTokenSpans.map(sentenceTokenSpans =>
+//      Tuple2(Span(sentenceTokenSpans.head._1.begin, sentenceTokenSpans.last._1.end), Sentence()))
+//
+//    // Flatten all the tokens into a single list
+//    val tokenSpans = sentenceAsTokenSpans.flatMap(t=> t)
+//
+//    // Create new Add annotations to a
+//    //if(start != slab.content.length)
+//      //spans += Span(start, slab.content.length) -> Token(slab.content.substring(start, slab.content.length))
+//    slab.addLayer[Sentence](sentenceSpans)
+//    slab.addLayer[Token](tokenSpans)
+//    slab
+//  }
+//}
--- src/test/scala/org/spakle/clearnlp/ClearNlpTest.scala
+++ src/test/scala/org/spakle/clearnlp/ClearNlpTest.scala
@@ -1,43 +1,45 @@
 package org.spakle.clearnlp
 
+import epic.slab.{Token, Sentence, Slab, StringSlab}
+import epic.trees.Span
 import org.scalatest.FunSuite
-import org.sparkle.clearnlp.ClearNlp
-import org.sparkle.slab.Slab
-import org.sparkle.typesystem.Span
-import org.sparkle.typesystem.basic.{Sentence, Token}
+import org.sparkle.clearnlp.{SentenceSegmenterAndTokenizer}
 
 class ClearNlpTest extends FunSuite {
 
   // =========
   // Analyzers
   // =========
-  import Slab.StringSlab
-
   val stringBegin = (slab: StringSlab[Span]) => slab
 
   // =========
   // Tests
   // =========
   test("ClearNLP sentence segmentation and tokenization test") {
-    val pipeline = stringBegin andThen ClearNlp.sentenceSegmenterAndTokenizer
+
+    val pipeline = SentenceSegmenterAndTokenizer //andThen PosTagger.posTagger
     val slab = pipeline(Slab( """This is sentence one.  Do you like sentence 2?  Mr. and Dr. Takahashi want to leave!  Go now!"""))
     val sentences = slab.iterator[Sentence].toList
-    assert(sentences.map(_.in(slab).content) === List(
+    assert(sentences.map{case (span, _) => slab.spanned(span)} === List(
       """This is sentence one.""",
       """Do you like sentence 2?""",
       """Mr. and Dr. Takahashi want to leave!""",
       """Go now!"""
     ))
 
-    val tokensInSentence0 = slab.covered[Token](sentences.head).toList
-    assert(tokensInSentence0.map(_.in(slab).content) === List("This", "is", "sentence", "one", "."))
+    val spanAnnotationToText = (spanAnnotationPair: Tuple2[Span, Any]) => spanAnnotationPair match {
+      case (span, _) => slab.spanned(span)
+    }
 
-    val tokensBeforeSentence1 = slab.preceding[Token](sentences(1)).toList
-    assert(tokensBeforeSentence1.map(_.in(slab).content) === tokensInSentence0.map(_.in(slab).content).reverse)
+    val tokensInSentence0 = slab.covered[Token](sentences.head._1).toList
+    assert(tokensInSentence0.map(spanAnnotationToText) === List("This", "is", "sentence", "one", "."))
+
+    val tokensBeforeSentence1 = slab.preceding[Token](sentences(1)._1).toList
+    assert(tokensBeforeSentence1.map(spanAnnotationToText) === tokensInSentence0.map(spanAnnotationToText).reverse)
 
     val tokens = slab.iterator[Token].toList
-    val tokensAfterToken18 = slab.following[Token](tokens(18)).toList
-    val tokensInSentence3 = slab.covered[Token](sentences(3)).toList
-    assert(tokensAfterToken18.map(_.in(slab).content) === tokensInSentence3.map(_.in(slab).content))
+    val tokensAfterToken18 = slab.following[Token](tokens(18)._1).toList
+    val tokensInSentence3 = slab.covered[Token](sentences(3)._1).toList
+    assert(tokensAfterToken18.map(spanAnnotationToText) === tokensInSentence3.map(spanAnnotationToText))
   }
 }
