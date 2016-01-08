6872c09ffea0456826bc8307aecaa6ea0ff04f59
leebecker <lee.becker@gmail.com>
2016-01-07 23:25:32 -0700

    Streamlining class definitions.
    Examples should really be ported to a testcase
    or elsewhere.  Commenting these out allows for
    removal of unrelated Tokenizers.

--- src/main/scala/epic/slab/AnalysisFunction.scala
+++ src/main/scala/epic/slab/AnalysisFunction.scala
@@ -1,18 +1,21 @@
 package epic.slab
 
-import epic.preprocess.{RegexSentenceSegmenter, Tokenizer}
 import epic.trees.Span
 
 /**
- * An analysis function that takes a Slab with declared annotation types in it and outputs
- * a new Slab with additional annotations of a new type.
- *
- * Documentation for the type variables:
- *   C = Content type
- *   B = Base annonation type
- *   I = Input annotation type
- *   O = Output annotation type
- */
+  * Adapted from scala-nlp's epic and chalk projects
+  */
+
+/**
+  * An analysis function that takes a Slab with declared annotation types in it and outputs
+  * a new Slab with additional annotations of a new type.
+  *
+  * Documentation for the type variables:
+  *   C = Content type
+  *   B = Base annonation type
+  *   I = Input annotation type
+  *   O = Output annotation type
+ **/
 trait AnalysisFunction[C,B,I,+O] {
   def apply[In <: I](slab: Slab[C,B,In]):Slab[C,B,In with O]
 
@@ -38,23 +41,10 @@ object StringIdentityAnalyzer extends StringAnalysisFunction[Any, Any] {
   def apply[In](slab: StringSlab[In]):StringSlab[In] = slab
 }
 
-/**
-  * A simple regex tokenizer.
-  */
-object RegexTokenizer extends Tokenizer {
-  def apply[I <: Sentence](slab: StringSlab[I]) =
-    // the [Token] is required because of https://issues.scala-lang.org/browse/SI-7647
-    slab.addLayer[Token](slab.iterator[Sentence].flatMap{ case (region, sentence) =>
-      "\\p{L}+|\\p{P}+|\\p{N}+".r.findAllMatchIn(slab.content.substring(region.begin, region.end)).map(m =>
-        Span(region.begin + m.start, region.begin + m.end) -> Token(m.group(0)))
-    })
-}
-
-
 object AnalysisPipeline {
   import AnnotatedSpan._
 
-  // added only to demonstrate necesssity of [I] parameter on analyzers
+  // added only to demonstrate necessity of [I] parameter on analyzers
   private[AnalysisPipeline] case class Document()
   private[AnalysisPipeline] def documentAdder(slab: StringSlab[Any]) =
     slab.addLayer(Iterator(Span(0, slab.content.length) -> Document()))
