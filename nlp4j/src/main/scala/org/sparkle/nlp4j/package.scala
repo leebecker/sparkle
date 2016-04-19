cb4f131906bf054dd58f566f6672ce5495b57a68
leebecker <lee.becker@gmail.com>
2016-04-19 15:44:13 -0600

    Adding wrappers for nlp4j tokenizer and sentence segmenter.

---
+++ nlp4j/src/main/scala/org/sparkle/nlp4j/package.scala
@@ -0,0 +1,13 @@
+package org.sparkle
+
+import org.sparkle.nlp4j.Nlp4jTokenizerWithSparkleTypes
+import edu.emory.mathcs.nlp.common.util.Language
+
+/**
+  * Created by leebecker on 4/19/16.
+  */
+package object nlp4j {
+  def tokenizer(language: Language = Language.ENGLISH) = new Nlp4jTokenizerWithSparkleTypes(language)
+
+  def sentenceSegmenterAndTokenizer(language: Language=Language.ENGLISH) = new Nlp4jSentenceSegmenterAndTokenizerWithSparkleTypes(language)
+}
