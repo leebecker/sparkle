e67da859e8d1cfdd1feebc409764e1d37b2a600f
Lee Becker <lee.becker@gmail.com>
2016-06-30 15:06:21 -0600

    Upgrading to NLP4J 1.1.1->1.1.2

--- nlp4j/build.sbt
+++ nlp4j/build.sbt
@@ -1,9 +1,9 @@
 lazy val nlp4jAndDependencies = Seq(
-  "edu.emory.mathcs.nlp" % "nlp4j" % "1.1.1",
-  "edu.emory.mathcs.nlp" % "nlp4j-core" % "1.1.1",
-  "edu.emory.mathcs.nlp" % "nlp4j-common" % "1.1.1",
-  "edu.emory.mathcs.nlp" % "nlp4j-tokenization" % "1.1.1",
-  "edu.emory.mathcs.nlp" % "nlp4j-morphology" % "1.1.1",
-  "edu.emory.mathcs.nlp" % "nlp4j-english" % "1.1.1"
+  "edu.emory.mathcs.nlp" % "nlp4j" % "1.1.2",
+  "edu.emory.mathcs.nlp" % "nlp4j-core" % "1.1.2",
+  "edu.emory.mathcs.nlp" % "nlp4j-common" % "1.1.2",
+  "edu.emory.mathcs.nlp" % "nlp4j-tokenization" % "1.1.2",
+  "edu.emory.mathcs.nlp" % "nlp4j-morphology" % "1.1.2",
+  "edu.emory.mathcs.nlp" % "nlp4j-english" % "1.1.2"
 )
 libraryDependencies ++= nlp4jAndDependencies
--- nlp4j/src/main/scala/org/sparkle/nlp4j/Nlp4jUtils.scala
+++ nlp4j/src/main/scala/org/sparkle/nlp4j/Nlp4jUtils.scala
@@ -7,8 +7,8 @@ import edu.emory.mathcs.nlp.common.collection.tree.PrefixTree
 import edu.emory.mathcs.nlp.common.collection.tuple.Pair
 import edu.emory.mathcs.nlp.component.template.feature.Field
 import edu.emory.mathcs.nlp.common.util.IOUtils
+import edu.emory.mathcs.nlp.component.template.lexicon.{GlobalLexica, GlobalLexicon}
 import edu.emory.mathcs.nlp.component.template.node.NLPNode
-import edu.emory.mathcs.nlp.component.template.util.GlobalLexica
 
 import collection.JavaConverters._
 
@@ -33,32 +33,47 @@ object Nlp4jUtils {
 
 
   def initAmbiguityClasses() = {
-    val ambiguityClasses = loadLexicon[java.util.Map[String, java.util.List[String]]](lexicaPrefix + "en-ambiguity-classes-simplified-lowercase.xz")
-    lexica.setAmbiguityClasses(new Pair(ambiguityClasses, Field.word_form_simplified_lowercase))
+    val ambiguityClasses = loadLexicon[java.util.Map[String, java.util.List[String]]](
+      lexicaPrefix + "en-ambiguity-classes-simplified-lowercase.xz",
+      Field.word_form_simplified_lowercase,
+      "ambiguity_classes")
+    lexica.setAmbiguityClasses(ambiguityClasses)
     true
   }
 
   def initWordClusters() = {
-    val wordClusters = loadLexicon[java.util.Map[String, java.util.Set[String]]](lexicaPrefix + "en-brown-clusters-simplified-lowercase.xz")
-    lexica.setWordClusters(new Pair(wordClusters, Field.word_form_simplified_lowercase))
+    val wordClusters = loadLexicon[java.util.Map[String, java.util.Set[String]]](
+      lexicaPrefix + "en-brown-clusters-simplified-lowercase.xz",
+      Field.word_form_simplified_lowercase,
+      "word_clusters")
+    lexica.setWordClusters(wordClusters)
     wordClusters
   }
 
   def initNamedEntityGazetteers() = {
-    val namedEntityGazetteers = loadLexicon[PrefixTree[String, java.util.Set[String]]](lexicaPrefix + "en-named-entity-gazetteers-simplified.xz")
-    lexica.setNamedEntityGazetteers(new Pair(namedEntityGazetteers, Field.word_form_simplified))
+    val namedEntityGazetteers = loadLexicon[PrefixTree[String, java.util.Set[String]]](
+      lexicaPrefix + "en-named-entity-gazetteers-simplified.xz",
+      Field.word_form_simplified,
+      "named_entity_gazetteers"
+    )
+    lexica.setNamedEntityGazetteers(namedEntityGazetteers)
     namedEntityGazetteers
   }
 
   def initWordEmbeddings() = {
-    val wordEmbeddings = Some(loadLexicon[java.util.Map[String, Array[Float]]](lexicaPrefix + "en-word-embeddings-undigitalized.xz"))
-    lexica.setWordEmbeddings(new Pair(wordEmbeddings.get, Field.word_form_undigitalized))
+    val wordEmbeddings = Some(loadLexicon[java.util.Map[String, Array[Float]]](
+      lexicaPrefix + "en-word-embeddings-undigitalized.xz",
+      Field.word_form_undigitalized,
+      "word_embeddings")
+    )
+    lexica.setWordEmbeddings(wordEmbeddings.get)
     wordEmbeddings
   }
 
-  def loadLexicon[T](aLocation: String) = {
+  def loadLexicon[T](aLocation: String, field: Field, name: String) = {
     val is = IOUtils.createObjectXZBufferedInputStream(getClass.getResourceAsStream(aLocation)).asInstanceOf[ObjectInputStream]
-    is.readObject.asInstanceOf[T]
+    val lexica = is.readObject.asInstanceOf[T]
+    new GlobalLexicon(lexica, field, name)
   }
 
 
