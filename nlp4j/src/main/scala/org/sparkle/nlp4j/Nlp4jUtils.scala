15e78a5ca2ae28cf39cfffe441896db747ed527e
Lee Becker <lee.becker@gmail.com>
2016-06-30 15:58:58 -0600

    Cleaning up inits and error handling for lexica.

--- nlp4j/src/main/scala/org/sparkle/nlp4j/Nlp4jUtils.scala
+++ nlp4j/src/main/scala/org/sparkle/nlp4j/Nlp4jUtils.scala
@@ -4,7 +4,6 @@ import java.io.ObjectInputStream
 import javax.xml.parsers.DocumentBuilderFactory
 
 import edu.emory.mathcs.nlp.common.collection.tree.PrefixTree
-import edu.emory.mathcs.nlp.common.collection.tuple.Pair
 import edu.emory.mathcs.nlp.component.template.feature.Field
 import edu.emory.mathcs.nlp.common.util.IOUtils
 import edu.emory.mathcs.nlp.component.template.lexicon.{GlobalLexica, GlobalLexicon}
@@ -22,6 +21,7 @@ object Nlp4jUtils {
   val builder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
   val xmlDoc = builder.newDocument()
   val root = xmlDoc.createElement("dummy")
+
   lazy val lexica = new GlobalLexica[NLPNode](root)
   // Needed for POS Tagging
   lazy val ambiguityClasses = initAmbiguityClasses()
@@ -36,9 +36,10 @@ object Nlp4jUtils {
     val ambiguityClasses = loadLexicon[java.util.Map[String, java.util.List[String]]](
       lexicaPrefix + "en-ambiguity-classes-simplified-lowercase.xz",
       Field.word_form_simplified_lowercase,
-      "ambiguity_classes")
-    lexica.setAmbiguityClasses(ambiguityClasses)
-    true
+      "ambiguity_classes"
+    )
+    if (ambiguityClasses.isDefined) lexica.setAmbiguityClasses(ambiguityClasses.get)
+    ambiguityClasses
   }
 
   def initWordClusters() = {
@@ -46,7 +47,7 @@ object Nlp4jUtils {
       lexicaPrefix + "en-brown-clusters-simplified-lowercase.xz",
       Field.word_form_simplified_lowercase,
       "word_clusters")
-    lexica.setWordClusters(wordClusters)
+    if (wordClusters.isDefined) lexica.setWordClusters(wordClusters.get)
     wordClusters
   }
 
@@ -56,24 +57,29 @@ object Nlp4jUtils {
       Field.word_form_simplified,
       "named_entity_gazetteers"
     )
-    lexica.setNamedEntityGazetteers(namedEntityGazetteers)
+    if (namedEntityGazetteers.isDefined) lexica.setNamedEntityGazetteers(namedEntityGazetteers.get)
     namedEntityGazetteers
   }
 
   def initWordEmbeddings() = {
-    val wordEmbeddings = Some(loadLexicon[java.util.Map[String, Array[Float]]](
+    val wordEmbeddings = loadLexicon[java.util.Map[String, Array[Float]]](
       lexicaPrefix + "en-word-embeddings-undigitalized.xz",
       Field.word_form_undigitalized,
-      "word_embeddings")
+      "word_embeddings"
     )
-    lexica.setWordEmbeddings(wordEmbeddings.get)
+
+    if (wordEmbeddings.isDefined) lexica.setWordEmbeddings(wordEmbeddings.get)
     wordEmbeddings
   }
 
   def loadLexicon[T](aLocation: String, field: Field, name: String) = {
-    val is = IOUtils.createObjectXZBufferedInputStream(getClass.getResourceAsStream(aLocation)).asInstanceOf[ObjectInputStream]
-    val lexica = is.readObject.asInstanceOf[T]
-    new GlobalLexicon(lexica, field, name)
+    try {
+      val is = IOUtils.createObjectXZBufferedInputStream(getClass.getResourceAsStream(aLocation))
+      val lexica = is.readObject.asInstanceOf[T]
+      Some(new GlobalLexicon(lexica, field, name))
+    } catch {
+      case e: Exception => None
+    }
   }
 
 
