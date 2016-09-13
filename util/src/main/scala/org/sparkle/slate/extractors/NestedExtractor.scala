e263a83e3970bd0183c5ecd3e0a744c098ec82f4
Lee Becker <lee.becker@gmail.com>
2016-09-13 03:33:33 -0600

    Fleshing out NestedExtractor functionality.

--- util/src/main/scala/org/sparkle/slate/extractors/NestedExtractor.scala
+++ util/src/main/scala/org/sparkle/slate/extractors/NestedExtractor.scala
@@ -3,19 +3,23 @@ package org.sparkle.slate.extractors
 import org.sparkle.slate._
 import org.sparkle.slate.StringSlateExtractor
 import org.sparkle.typesystem.basic.{Sentence, Token}
+import org.sparkle.typesystem.syntax.dependency._
 
 
-case class SentenceSchema(beginIndex: Int, endIndex: Int, tokens: Seq[TokenSchema])
+// By convention -1 is the top node
+case class DependencyRelationSchema(relation:String, tokenIdx: Int, headTokenIdx: Int)
 
-case class TokenSchema(token: String, pos: String, beginIndex: Int, endIndex: Int)
+case class TokenSchema(token: String, pos: String, lemma: String, beginIndex: Int, endIndex: Int)
 
-case class ResultSchema(text: String, sentences: Seq[SentenceSchema])
+case class SentenceSchema(beginIndex: Int, endIndex: Int, tokens: Seq[TokenSchema], dependencyParse: Seq[DependencyRelationSchema])
+
+case class NestedExtractorAnalyses(sentences: Seq[SentenceSchema])
 
 
 /**
-  * Created by leebecker on 4/18/16.
+  *
   */
-object NestedExtractor extends StringSlateExtractor[ResultSchema] with Serializable {
+object NestedExtractor extends StringSlateExtractor[NestedExtractorAnalyses] with Serializable {
 
   def apply(slate: StringSlate) = {
 
@@ -26,12 +30,52 @@ object NestedExtractor extends StringSlateExtractor[ResultSchema] with Serializa
         val (spansAndTokens) = slate.covered[Token](sentenceSpan)
 
         val tokens = spansAndTokens.map {
-          case (tokenSpan, token) => TokenSchema(token.token, token.pos.orNull, tokenSpan.begin, tokenSpan.end)
+          case (tokenSpan, token) => TokenSchema(token.token, token.pos.orNull, token.lemma.orNull, tokenSpan.begin, tokenSpan.end)
         }
-        SentenceSchema(sentenceSpan.begin, sentenceSpan.end, tokens)
+
+        val tokenSpanToTokenIndex = spansAndTokens.map(_._1).zipWithIndex.toMap
+
+        val dependencyRelations = slate.
+          covered[DependencyRelation](sentenceSpan).
+          flatMap {
+            case (depRelSpan, depRel) => {
+              (depRel.dependent, depRel.head) match {
+                case (node, head: LeafDependencyNode) =>
+                  val nodeIdx = tokenSpanToTokenIndex(node.span)
+                  val headIdx = tokenSpanToTokenIndex(head.span)
+                  val rel = depRel.relation
+                  Some(DependencyRelationSchema(rel, nodeIdx, headIdx))
+                case (node, head: RootDependencyNode) =>
+                  val nodeIdx = tokenSpanToTokenIndex(node.span)
+                  val headIdx = -1
+                  val rel = depRel.relation
+                  Some(DependencyRelationSchema(rel, nodeIdx, headIdx))
+                case _ => None
+              }
+            }
+          }
+
+
+
+        val x = spansAndTokens.zipWithIndex.flatMap {
+          case ((tokenSpan, token), tokenIdx) => {
+            val node = slate.covered[DependencyNode](tokenSpan).headOption
+            if (node.isDefined) {
+
+            }
+            node
+          }
+        }
+
+        tokens.zipWithIndex.map {
+          t=> t
+          //case ((tokenSpan, token), tokenIdx) => slate.covered[DependencyNode]
+
+        }
+
+        SentenceSchema(sentenceSpan.begin, sentenceSpan.end, tokens, dependencyRelations)
       }
     }
-    ResultSchema(text = slate.content, sentences = sentenceStructs)
-
+    NestedExtractorAnalyses(sentences = sentenceStructs)
   }
 }
